81faaf5350bfed3125730a626e00bfa839c9ab17
Kondrat22 <kondrat_va@bk.ru>
2024-08-13 16:20:57 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -9,17 +9,17 @@ def calculate_structure_sum(data_structure):
     elif isinstance(data_structure, (list, tuple, set)):
         for item in data_structure:
             summa += calculate_structure_sum(item)
-            # иначе выполняеться действие 3 add к summa - целое число и число с плавающей запятой
+            
     elif isinstance(data_structure, (int, float)):
         summa += data_structure
-        # иначе выполняеться действие 4 add k summa - фу-цию len
+       
     elif isinstance(data_structure, str):
         summa += len(data_structure)
-        # возврат из функции значения summa
+       
     return summa
 
 
-# данные взятые из условия задачи
+
 data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
@@ -27,6 +27,6 @@ data_structure = [
     "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-# вывод результата программы
+
 result = calculate_structure_sum(data_structure)
 print(result)
